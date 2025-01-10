accd4a50b3a722bd0668df138b5700731b3beb11
NguyenDat0809 <datngx.dev@gmail.com>
2025-01-10 10:55:17 +0700

    feat: build static skin test ui
    6 question, each question has 4 answers

--- SpaVuiVe/SpaVuiVe_FE/Pages/SkinTestPages/Index.cshtml.cs
+++ SpaVuiVe/SpaVuiVe_FE/Pages/SkinTestPages/Index.cshtml.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Models;
 
 namespace SpaVuiVe_FE.Pages.SkinTestPages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+
+        public IndexModel()
         {
+          
+        }
+
+ 
+
+        public async Task OnGetAsync()
+        {
+           
         }
     }
 }
