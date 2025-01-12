acb937f7a0a366c7d42ddeee416d25a9d65c7080
bacsiemon <johnsieunhan816@gmail.com>
2025-01-12 15:04:58 +0700

    login & register

---
+++ SpaVuiVe/SpaVuiVe_FE/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpaVuiVe_FE.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SpaVuiVe/SpaVuiVe_FE/Pages/Login/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpaVuiVe_FE.Pages.Login
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
