cc6ab2ebb75175a652a894e4d4e555fb7bbb73fd
Nhật Minh <trannguyennhatminh435@gmail.com>
2025-01-18 14:18:50 +0700

    fix brand, order, order detail, payment, payment method services, controller; add redis

---
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Api/Controllers/PaymentMethodController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkincareProductSalesSystem.Services;
+
+namespace SkincareProductSalesSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentMethodController : ControllerBase
+    {
+        private readonly IPaymentMethodServices _paymentMethodServices;
+
+        public PaymentMethodController(IPaymentMethodServices paymentMethodServices)
+        {
+            _paymentMethodServices = paymentMethodServices;
+        }
+
+        [HttpGet("/payment-methods")]
+        public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int size = 10)
+        {
+            var responses = await _paymentMethodServices.GetAll(page, size);
+            return (responses != null)? Ok(responses) : StatusCode(500);
+        }
+
+        [HttpGet("/payment-methods/{id}")]
+        public async Task<IActionResult> GetPaymentMethodById(string id)
+        {
+            var response = await _paymentMethodServices.GetPaymentMethodById(id);
+            return (response != null)? Ok(response) : NotFound();
+        }
+    }
+}
--- SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Api/Program.cs
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Api/Program.cs
@@ -1,9 +1,32 @@
+using SkincareProductSalesSystem.Repositories.Database;
+using SkincareProductSalesSystem.Repositories.Repositories;
+using SkincareProductSalesSystem.Repositories;
+using SkincareProductSalesSystem.Services;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystemDBContext>();
+builder.Services.AddScoped<OrderRepository>();
+builder.Services.AddScoped<BrandRepository>();
+builder.Services.AddScoped<OrderDetailRepository>();
+builder.Services.AddScoped<PaymentMethodRepository>();
+builder.Services.AddScoped<PaymentRepository>();
+builder.Services.AddScoped<IOrderServices, OrderServices>();
+builder.Services.AddScoped<IOrderDetailServices, OrderDetailServices>();
+builder.Services.AddScoped<IBrandService, BrandServices>();
+builder.Services.AddScoped<IPaymentServices, PaymentServices>();
+builder.Services.AddScoped<IPaymentMethodServices, PaymentMethodServices>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
