6afa1c71e0cdc858e9a990cc990ed687cb2d2dc5
NguyenDat0809 <datngx.dev@gmail.com>
2025-02-01 09:22:01 +0700

    feat: crud for skin test api

---
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Api/Controllers/SkinController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkincareProductSalesSystem.Services;
+
+namespace SkincareProductSalesSystem.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkinController : ControllerBase
+    {
+        private readonly ISkinTestService _skinTestService;
+        private readonly ISkinTypeService _skinTypeService;
+
+        public SkinController(ISkinTestService skinTestService, ISkinTypeService skinTypeService )
+        {
+            _skinTestService = skinTestService;
+            _skinTypeService = skinTypeService;
+        }
+
+        [HttpGet("/skin-tests")]
+        public IActionResult GetAllSkinTest()
+        {
+            var responses = _skinTestService.GetAllAsync();
+            return (responses != null) ? Ok(responses.Result) : StatusCode(500);
+        }
+
+        [HttpGet("/skin-tests/{id}")]
+        public IActionResult GetById(string id)
+        {
+            var response = _skinTestService.GetAsync(id);
+            return (response != null) ? Ok(response.Result) : NotFound();
+        }
+        [HttpPost("/skin-tests")]
+        public IActionResult CreateSkinTestQuestion(CreateSkinTestRequest request)
+        {
+            var response = _skinTestService.Create(request);
+            return (response != null) ? Ok(response.Result) : NotFound();
+        }
+        [HttpPut("/skin-tests/{id}")]
+        public IActionResult UpdatekinTestQuestion(string id, UpdateSkinTestRequest request)
+        {
+            var response = _skinTestService.Update(id, request);
+            return (response != null) ? Ok(response.Result) : NotFound();
+        }
+        [HttpDelete("/skin-tests/{id}")]
+        public IActionResult DeleteSkinTestQuestion(string id)
+        {
+            var response = _skinTestService.Delete(id);
+            return (response != null) ? Ok(response.Result) : NotFound();
+        }
+    }
+
+}
