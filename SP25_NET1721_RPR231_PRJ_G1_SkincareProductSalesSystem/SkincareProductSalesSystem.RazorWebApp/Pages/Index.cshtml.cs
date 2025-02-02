d0e5fb79b8e68aedb21c3fe508a2b6940db41bd5
DatSung <datsung.dev@gmail.com>
2025-02-02 17:22:36 +0700

    feat: get categories and display on homepage

--- SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.RazorWebApp/Pages/Index.cshtml.cs
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.RazorWebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SkincareProductSalesSystem.Common;
+using SkincareProductSalesSystem.RazorWebApp.Models;
+using SkincareProductSalesSystem.RazorWebApp.Models.Base;
+using Newtonsoft.Json;
+using SkincareProductSalesSystem.Common.Utils;
+using SkincareProductSalesSystem.Repositories.Paginate;
 
 namespace SkincareProductSalesSystem.RazorWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApiClient _apiClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public IndexModel(ApiClient apiClient)
         {
-            _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            var result = await _apiClient.GetAsync("/categories?page=1&size=5");
 
+            if (result.Status == 200 && result.Data != null)
+            {
+                var paginateCategories = JsonConvert.DeserializeObject<Paginate<Category>>(result.Data.ToString());
+                Categories = paginateCategories.Items;
+            }
+            else
+            {
+                Categories = new List<Category>();
+            }
         }
     }
 }
