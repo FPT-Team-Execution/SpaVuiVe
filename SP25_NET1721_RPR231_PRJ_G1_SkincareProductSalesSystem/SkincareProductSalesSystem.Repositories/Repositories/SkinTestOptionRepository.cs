b1330a6d8888323e66d28a9f09bcf73fb8219a39
NguyenDat0809 <datngx.dev@gmail.com>
2025-02-06 21:02:32 +0700

    update: add skin test option repo

---
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Repositories/Repositories/SkinTestOptionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkincareProductSalesSystem.Repositories.Base;
+using SkincareProductSalesSystem.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkincareProductSalesSystem.Repositories.Repositories
+{
+    public class SkinTestOptionRepository : GenericRepository<SkinTestOption>
+    {
+        public new async Task<SkinTestOption?> GetByIdAsync(string id)
+        {
+            return await _context.Set<SkinTestOption>().FindAsync(id);
+        }
+    }
+}
