afe0a0a9ed851c8b591783d3be1a4b203be0eb84
NguyenDat0809 <datngx.dev@gmail.com>
2025-02-06 21:00:23 +0700

    update: ef power tool after modify db
    add skin test option

--- SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Repositories/Repositories/SkinTestRepository.cs
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Repositories/Repositories/SkinTestRepository.cs
@@ -3,11 +3,11 @@ using SkincareProductSalesSystem.Repositories.Models;
 
 namespace SkincareProductSalesSystem.Repositories
 {
-    public class SkinTestRepository : GenericRepository<SkinTest>
+    public class SkinTestRepository : GenericRepository<SkinTestQuestion>
     {
-        public new async Task<SkinTest?> GetByIdAsync(string id)
+        public new async Task<SkinTestQuestion?> GetByIdAsync(string id)
         {
-            return await _context.Set<SkinTest>().FindAsync(id);
+            return await _context.Set<SkinTestQuestion>().FindAsync(id);
         }
     }
 }
