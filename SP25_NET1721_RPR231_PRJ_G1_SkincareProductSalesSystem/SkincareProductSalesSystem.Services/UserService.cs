a424ac788083044d8a00b0807f52f11800bd9047
dell <dell@DESKTOP-42P6OJP>
2025-02-09 11:42:53 +0700

    9/2

---
+++ SP25_NET1721_RPR231_PRJ_G1_SkincareProductSalesSystem/SkincareProductSalesSystem.Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkincareProductSalesSystem.Repositories.Models;
+
+namespace SkincareProductSalesSystem.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAll();
+        Task<User?> GetUserById(string id);
+        Task<User?> CreateUser(User user);
+        Task<User?> UpdateUser(User user);
+        Task<User?> DeleteUser(string id);
+    }
+    public class UserService : IUserService
+    {
+        public Task<User?> CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> DeleteUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
